fda0905217b100598e251953ef974ff7ad84a321
pes18fan <pes18fan@disr.it>
2023-04-13 20:23:49 +0545

    [refactor] move to mdx from md

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig } from 'astro/config';
 
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 export default defineConfig({
-    markdown: {
-        shikiConfig: {
-            theme: "monokai"
-        }
+  markdown: {
+    shikiConfig: {
+      theme: "monokai"
     }
+  },
+  integrations: [mdx()]
 });
