194773f223275d6135ba54acd41b969e1bc63d18
pes18fan <pes18fan@disr.it>
2023-05-13 10:49:14 +0545

    [post] add "getting rid of semicolons"

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 # site
 
 Personal site. Created using the Astro static site generator. 
-
-Aims to have zero or as low JavaScript as possible on the client.
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,23 @@
+import { readFileSync } from "fs"
 import { defineConfig } from 'astro/config';
 
 import mdx from "@astrojs/mdx";
+
+const kazeGrammar = JSON.parse(readFileSync("./public/assets/kaze.tmLanguage.json"));
 
 // https://astro.build/config
 export default defineConfig({
   markdown: {
     shikiConfig: {
-      theme: "monokai"
+      theme: "monokai",
+      langs: [
+        {
+          id: "kaze",
+          scopeName: "source.kaze",
+          grammar: kazeGrammar,
+          aliases: ["kaze"]
+        }
+      ]
     }
   },
   integrations: [mdx()],
