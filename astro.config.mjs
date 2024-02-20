9f40a044a7af1e78f3933289dde801c55839de94
pes18fan <pes18fan@disr.it>
2024-02-20 22:48:09 +0545

    [add] analytics
    If you don't want to be tracked, feel free to use a content blocker.
    I recommend uBlock Origin.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import { readFileSync } from "fs"
 import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel/serverless';
 
 import mdx from "@astrojs/mdx";
 
@@ -7,6 +8,11 @@ const kazeGrammar = JSON.parse(readFileSync("./public/assets/kaze.tmLanguage.jso
 
 // https://astro.build/config
 export default defineConfig({
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    }
+  }),
   markdown: {
     shikiConfig: {
       theme: "monokai",
