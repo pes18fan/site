d3f25601618518822aa79deb1bc8ea12c60109c8
pes18fan <pes18fan@disr.it>
2024-02-20 22:55:22 +0545

    [fix] vercel analytics stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,31 +1,25 @@
-import { readFileSync } from "fs"
+import { readFileSync } from "fs";
 import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel/serverless';
-
 import mdx from "@astrojs/mdx";
-
 const kazeGrammar = JSON.parse(readFileSync("./public/assets/kaze.tmLanguage.json"));
+
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: vercel({
-    webAnalytics: {
-      enabled: true,
-    }
-  }),
+  adapter: vercel(),
   markdown: {
     shikiConfig: {
       theme: "monokai",
-      langs: [
-        {
-          id: "kaze",
-          scopeName: "source.kaze",
-          grammar: kazeGrammar,
-          aliases: ["kaze"]
-        }
-      ]
+      langs: [{
+        id: "kaze",
+        scopeName: "source.kaze",
+        grammar: kazeGrammar,
+        aliases: ["kaze"]
+      }]
     }
   },
   integrations: [mdx()],
-  site: "https://p18f.vercel.app"
+  site: "https://p18f.vercel.app",
+  output: "server"
 });
