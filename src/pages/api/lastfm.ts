52bb25aa9f259b2bbc3df795724a7393db5d1be3
pes18fan <pes18fan@disr.it>
2024-03-07 13:32:45 +0545

    feat: added caching for the lastfm api

--- src/pages/api/lastfm.ts
+++ src/pages/api/lastfm.ts
@@ -8,6 +8,23 @@ const rateLimiter = new RateLimiterMemory({
     duration: 10,
 })
 
+const cache = {
+    artist: "starting...",
+    track: "beep boop",
+    playing: true,
+};
+let cachedDate: ReturnType<typeof Date.now> = 0;
+
+const username = "***REMOVED***";
+const apiKey = import.meta.env.LASTFM_API_KEY;
+
+const url = new URL("https://ws.audioscrobbler.com/2.0");
+url.searchParams.set("method", "user.getRecentTracks");
+url.searchParams.set("user", username);
+url.searchParams.set("api_key", apiKey);
+url.searchParams.set("format", "json");
+url.searchParams.set("limit", "1");
+
 export const GET: APIRoute = async ({ request }) => {
     try {
         const key = request.headers.get("x-forwarded-for") || request.headers.get("cf-connecting-ip");
@@ -16,16 +33,25 @@ export const GET: APIRoute = async ({ request }) => {
         return new Response("Too many requests", { status: 429 });
     }
     
-    const username = "***REMOVED***";
-    const apiKey = import.meta.env.LASTFM_API_KEY;
-    const url = `https://ws.audioscrobbler.com/2.0/?method=user.getRecentTracks&user=${username}&api_key=${apiKey}&format=json`;
+    if (Date.now() - cachedDate > 1000 * 60) {
+        await updateCache();
+    }
 
-    const response = await fetch(url);
-    const data = await response.json();
-
-    return new Response(JSON.stringify(data), {
+    return new Response(JSON.stringify(cache), {
         headers: {
             "content-type": "application/json",
         },
     });
 };
+
+async function updateCache() {
+    const response = await fetch(url);
+    const data = await response.json();
+
+    const track = data.recenttracks.track[0];
+    cache.artist = track.artist["#text"];
+    cache.track = track.name;
+    cache.playing = track["@attr"]?.nowplaying === "true";
+
+    cachedDate = Date.now();
+}
