b7711768c3b0f135553ce7bbe19952beae6f500e
pes18fan <pes18fan@disr.it>
2024-04-10 11:54:09 +0545

    chore: change code theme

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,23 +4,30 @@ import mdx from "@astrojs/mdx";
 import svelte from "@astrojs/svelte";
 import vercel from "@astrojs/vercel/serverless";
 
-const kazeGrammar = JSON.parse(readFileSync("./public/assets/kaze.tmLanguage.json"));
+const kazeGrammar = JSON.parse(
+  readFileSync("./public/assets/kaze.tmLanguage.json"),
+);
 
 // https://astro.build/config
 export default defineConfig({
   markdown: {
     shikiConfig: {
-      theme: "monokai",
-      langs: [{
-        id: "kaze",
-        scopeName: "source.kaze",
-        grammar: kazeGrammar,
-        aliases: ["kaze"]
-      }, "javascript", "lua", "crystal"]
-    }
+      theme: "catppuccin-mocha",
+      langs: [
+        {
+          id: "kaze",
+          scopeName: "source.kaze",
+          grammar: kazeGrammar,
+          aliases: ["kaze"],
+        },
+        "javascript",
+        "lua",
+        "crystal",
+      ],
+    },
   },
   integrations: [mdx(), svelte()],
   site: "https://p18f.vercel.app",
   output: "hybrid",
-  adapter: vercel()
+  adapter: vercel(),
 });
