e06ab5958dfa49a8c52dad5839e21d9be6bced87
pes18fan <pes18fan@disr.it>
2024-04-20 14:15:42 +0545

    remove astro db

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,31 +3,25 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import svelte from "@astrojs/svelte";
 import vercel from "@astrojs/vercel/serverless";
+import db from "@astrojs/db";
+const kazeGrammar = JSON.parse(readFileSync("./public/assets/kaze.tmLanguage.json"));
 
-const kazeGrammar = JSON.parse(
-  readFileSync("./public/assets/kaze.tmLanguage.json"),
-);
 
 // https://astro.build/config
 export default defineConfig({
   markdown: {
     shikiConfig: {
       theme: "catppuccin-mocha",
-      langs: [
-        {
-          id: "kaze",
-          scopeName: "source.kaze",
-          grammar: kazeGrammar,
-          aliases: ["kaze"],
-        },
-        "javascript",
-        "lua",
-        "crystal",
-      ],
-    },
+      langs: [{
+        id: "kaze",
+        scopeName: "source.kaze",
+        grammar: kazeGrammar,
+        aliases: ["kaze"]
+      }, "javascript", "lua", "crystal"]
+    }
   },
-  integrations: [mdx(), svelte()],
+  integrations: [mdx(), svelte(), db()],
   site: "https://p18f.vercel.app",
   output: "hybrid",
-  adapter: vercel(),
+  adapter: vercel()
 });
