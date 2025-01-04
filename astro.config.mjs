a726baaed89167be83afd6f9ede873f0f1287f9f
pes18fan <pes18fan@disr.it>
2025-01-04 10:33:27 +0545

    chore: upgrade to astro 5, svelte 5, etc

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,33 +2,32 @@ import { readFileSync } from "fs";
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import svelte from "@astrojs/svelte";
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel";
 
 const kazeGrammar = JSON.parse(
-  readFileSync("./public/assets/kaze.tmLanguage.json"),
+    readFileSync("./public/assets/kaze.tmLanguage.json"),
 );
 
 // https://astro.build/config
 export default defineConfig({
-  markdown: {
-    shikiConfig: {
-      theme: "catppuccin-mocha",
-      langs: [
-        {
-          id: "kaze",
-          scopeName: "source.kaze",
-          grammar: kazeGrammar,
-          aliases: ["kaze"],
+    markdown: {
+        shikiConfig: {
+            theme: "catppuccin-mocha",
+            langs: [
+                {
+                    id: "kaze",
+                    scopeName: "source.kaze",
+                    grammar: kazeGrammar,
+                    aliases: ["kaze"],
+                },
+                "javascript",
+                "lua",
+                "crystal",
+            ],
         },
-        "javascript",
-        "lua",
-        "crystal",
-      ],
     },
-  },
-  integrations: [mdx(), svelte()],
-  site: "https://p18f.vercel.app",
-  output: "hybrid",
-  adapter: vercel(),
+    integrations: [mdx(), svelte()],
+    site: "https://p18f.vercel.app",
+    adapter: vercel(),
 });
 
