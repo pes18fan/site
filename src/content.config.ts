f94fcfa1003179149b194f08a80da740f29808a0
pes18fan <pes18fan@disr.it>
2025-01-04 10:33:27 +0545

    chore: upgrade to astro 5, svelte 5, etc

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,6 +1,8 @@
 import { z, defineCollection } from "astro:content";
+import { glob } from "astro/loaders";
 
 const postCollection = defineCollection({
+    loader: glob({ pattern: "**/[^_]*.{md,mdx}", base: "./src/content/posts" }),
     schema: z.object({
         title: z.string(),
         author: z.string().default("anon"),
